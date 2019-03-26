a7e9017d3691e49a4506f865f14229905c71bf92
Megan Huff <meganahuff@gmail.com>
2019-03-26 19:50:55 -0400

    Updates

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
+const listitems = document.getElementsByClassName ('nav')
+
+listitems.addEventListener('mouseover',() => {
+  listitems.textContent = listItems.textContent.toUpperCase();
+});
+
+
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-
-var menu = dropdown;
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle('show');
+    document.getElementById("myDropdown").classList.toggle("show");
   }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.menu')) {
+      var dropdowns = document.getElementsByClassName("menu-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
 
