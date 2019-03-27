af66c834d545d957cb9d9252f4c41013ab93216d
Megan Huff <meganahuff@gmail.com>
2019-03-26 22:26:26 -0400

    Updates

--- js/script.js
+++
@@ -1,8 +0,0 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-
-var menu = dropdown;
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle('show');
-  }
-
